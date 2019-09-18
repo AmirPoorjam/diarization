3e63b6438dd3d3d67dfc540df3f07592d3488d80
Amir <a.h.poorjam@gmail.com>
2019-09-18 15:52:02 +0200

    Garbage channel generation has been commented out

--- diarization_ihmm.py
+++ diarization_ihmm.py
@@ -77,8 +77,8 @@ varname_ch_1 = destinationfolder + filename[0:-4] + '_ch_1.wav'
 librosa.output.write_wav(varname_ch_0, diarized_signal[ind_vs[0,-2]], fs)
 librosa.output.write_wav(varname_ch_1, diarized_signal[ind_vs[0,-1]], fs)
 
-varname_ch_2 = destinationfolder + filename[0:-4] + '_ch_g.wav'
-librosa.output.write_wav(varname_ch_2, diarized_signal[ind_vs[0,-3]], fs)
+# varname_ch_2 = destinationfolder + filename[0:-4] + '_ch_g.wav'
+# librosa.output.write_wav(varname_ch_2, diarized_signal[ind_vs[0,-3]], fs)
 
 toc = time.time()
 print('|-------------------------------------------------')
@@ -89,5 +89,5 @@ print('| Signal duration: %2.2f min.' % (s.shape[0]/(fs*60)))
 print('| Processing time: %2.2f sec.' % (toc-tic))
 print('| Relative processing time: %2.2f %% of the signal duration.' % (100*(toc-tic)/(s.shape[0]/fs)))
 print('|-------------------------------------------------')
-#return diarized_signal
+
 
